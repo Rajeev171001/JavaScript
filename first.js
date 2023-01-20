3b2d3f48dfc1e05a143413d4db2977b622e4e6b4
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-20 23:30:13 +0530

    Tut 1

---
+++ first.js
@@ -0,0 +1,4 @@
+alert("hello world");
+// document.getElementById("hel").innerHTML="Rajeev";
+document.write("hello"); //write does not used when defer is used in script
+console.log("Hello Rajeev") // executed in console
