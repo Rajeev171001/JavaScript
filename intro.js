81b5bff6d0720050a9bf7920982d9e31eb4e428a
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-22 23:19:52 +0530

    introduction

---
+++ intro.js
@@ -0,0 +1,17 @@
+//print
+// console.log("Hello");
+// //variable
+// //declaration of variable
+// let a;
+// a=10;
+// a="hey";
+// // jS tell only variable not the type
+// console.log(a); // deafault variable is undefined // jo last me update hoga vhi rehga
+
+//variable type  primitive: number, string, boolean, null
+
+//normal code
+let number=10;
+for(let i=1;i<=number;i++){
+    console.log("number is:",i);
+}
