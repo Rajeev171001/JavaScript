72c1ae59212a3ef9d787d73fab26b8934d81016a
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-22 23:20:32 +0530

    Function

---
+++ function.js
@@ -0,0 +1,14 @@
+// NOn primitive => array, objects, functions
+
+//function define
+function sayHi(para){
+    // console.log("Hello");
+    // console.log("para is:",para);
+    return "returned from function";
+}
+//function call
+// sayHi(10);
+// sayHi("hello");
+// sayHi([1,2,3,4]);
+let rev=sayHi([1,2,3,4]);
+console.log("rev",rev);
