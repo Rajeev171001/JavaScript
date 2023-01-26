87a6da77726c090b5ab9d5da50116fff1e2677c2
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-26 15:13:45 +0530

    Arrays, Objects, Functoins

---
+++ objects_Arrays_function.js
@@ -0,0 +1,53 @@
+// objects
+// key:val
+// String, number: valid typeof
+
+// let object={
+//     name:"Rajeev",
+//     surname:"singh",
+//     sayHi:function (){
+//         console.log("Rajeev say's HI");
+//     }
+// }
+// //loop
+// for(let key in object){
+//     console.log(key," :",object[key]);
+// }
+// console.log(object);
+// object.sayHi();
+
+//Arrays
+
+// let arr=[1,2,3,4,5];
+// arr.myprop="Hello";
+// arr.Print=function(){
+//     console.log("hello from array");
+// }
+// for(let key in arr){
+//     console.log(key,":",arr[key]);
+// }
+// console.log(arr);
+
+
+//Function
+// function -> obkect -> key:IDBCursorWithValue
+// extra feature -> code property that can be executed when invoke it  ( code property-> code inside function)
+
+// function fn(){
+//     console.log("hello from fn");
+// }
+// fn.group="property of function";
+// fn.myFn=function fn(){
+//     console.log("i m method of a function");
+
+// }
+// console.log(fn);
+// fn();
+// fn.myFn();
+// console.log(fn.group);
+
+//JS -> primitive or objects
+// everything object -> DataTransfer, errors, modules
+// 6 primitives types -> Number, string, boolean, undefined null, symbol
+
+
