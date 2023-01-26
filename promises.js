c16f43464c89bcabd4dbfa63c329868b8fc516dd
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-26 16:32:46 +0530

    Pzromise

---
+++ promises.js
@@ -0,0 +1,22 @@
+//promise -> it give promise that function will run in future
+
+let fs=require("fs");
+//async code
+console.log("Before");
+// fs.readFile("f1.txt",function(err,data)){
+//     console.log(data);
+// });
+
+let promise=fs.promises.readFile("F1.txt");
+console.log(promise);
+//setteled
+
+//if promise setteled and fullfilled
+promise.then(function(data){
+    console.log(""+data);
+})
+//if promise rejected
+promise.catch(function(err){
+    console.log(err);
+})
+console.log("after");
