6feae236db38c2642fc6b850d23f5acb0b62c03e
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-26 13:51:08 +0530

    Variable scope

---
+++ var_scope.js
@@ -0,0 +1,40 @@
+// console.log("line 1",varName);
+// var varName;//declare
+// varName=10;//assign
+// console.log("line 4",varName);
+// varName=20;//reassign
+// console.log("line 6",varName);
+// var varName;//can declare again
+// console.log("line 8",varName);
+
+
+// function fn(){
+//     console.log(a);
+//     var a=10;
+//     console.log(a);
+//     if(a==10){
+//         var a=20;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// fn();
+
+// var a=10;
+// console.log("line 24",a);
+// function fn(){
+//     console.log("line 26",a);
+//     var a=20;
+//     a++;
+//     console.log("line 29",a);
+//     if(a){
+//         var a=30;
+//         a++;
+//         console.log("line 33",a);
+//     }
+//     console.log("line 35",a);
+// }
+// fn();
+// console.log("line 38",a);  // memory of function se alag g global stack me 10 h
+
+
