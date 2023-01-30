06cc24f02e7a7804afee3ed40ec344f1b5fb7cf0
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-01-30 15:30:26 +0530

    immperative and declerative function

---
+++ immperative_vs_declarative.js
@@ -0,0 +1,20 @@
+// is the square of a nunber is even
+
+//imperative
+// const x=5;
+// const xsquare=x*x;
+
+// let isEven;
+// if(xsquare%2==0){
+//     isEven=true;
+// }else{
+//     isEven=false;
+// }
+// console.log(isEven)
+
+
+//declerative
+
+const isSquareEven = (x)=>((x*x)%2==0?true:false)
+//take value of x it do square and check
+console.log(isSquareEven(4));
